7d37d312c56fdad5765181add1b5fe02ba6878ec
Никита <@DESKTOP-0N74OFC>
2023-09-29 11:07:39 +0700

    Добавьте файлы проекта.

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include<locale>
+using namespace std;
+
+// Тугарин Никита ИВТ-31. Задание 19: Три точки на числовой оси a, b, c, где a < c < b, найти произведение длин отрезков ac и bc
+int main() {
+	setlocale(LC_ALL, "ru");
+	int a, b, c;
+	cout << "\n Введите 3 числа a,b,c, при этом a<c<b";
+	cin >> a>>b>>c;
+	int ac = a * c;
+	int bc = b * c;
+	cout << ac << " " << bc;
+}
